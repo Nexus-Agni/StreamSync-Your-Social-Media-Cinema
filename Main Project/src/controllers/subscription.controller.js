42a8420a5b8c52ab7f3c5de6c27d3850c3639684
Nexus-Agni <agnibhachakraborty11@gmail.com>
2025-11-21 10:30:50 +0530

    Refactor subscription routes; swap user channel subscribers and subscribed channels endpoints for better clarity and functionality.

--- Main Project/src/controllers/subscription.controller.js
+++ Main Project/src/controllers/subscription.controller.js
@@ -52,12 +52,125 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
+    try {
+        const {channelId} = req.params;
+        if (!channelId || !isValidObjectId(channelId)) {
+            throw new ApiError(400, "Valid ChannelId is required")
+        }
+
+        const userId = req.user?._id;
+        if (userId.toString() !== channelId.toString()) {
+            throw new ApiError(403, "Forbidden: You can only get subscriber list of your own channel")
+        }
+    
+        const subscriberList = await Subscription.aggregate([
+            {
+                $match : {
+                    channel : new mongoose.Types.ObjectId(channelId)
+                }
+            }, {
+                $lookup : {
+                    from : "users",
+                    localField : "subscriber",
+                    foreignField : "_id",
+                    as : "subscriberInfo", 
+                    pipeline : [
+                        {
+                            $project : {
+                                _id : 1,
+                                username : 1,
+                                fullname : 1,
+                                avatar : 1
+                            }
+                        }
+                    ]
+                }, 
+            }, {
+                $unwind : "$subscriberInfo"
+            },  {
+                $project : {
+                    _id : 1,
+                    subscriber : 1,
+                    channel : 1,
+                    subscriberInfo : "$subscriberInfo"
+                }
+            }
+        ])
+    
+        if (!subscriberList) {
+            throw new ApiError(500, "Unable find to find subscriber list")
+        }
+
+        const subscriberCount = subscriberList.length;
+    
+        res.status(200).json(
+            new ApiResponse(200, {
+                subscriberCount,
+                subscriberList
+            }, "Subscriber list fetched successfully" )
+        )
+    } catch (error) {
+        throw new ApiError(error.statusCode || 500, error.message || "Something went wrong while fetching subscriber list")
+    }
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
+try {
     const { subscriberId } = req.params
+    if (!subscriberId || !isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "Valid SubscriberId is required")
+    }
+
+    const subscribedChannels = await Subscription.aggregate([
+        {
+            $match : {
+                subscriber : new mongoose.Types.ObjectId(subscriberId)
+            }
+        }, {
+            $lookup : {
+                from : "users",
+                localField : "channel",
+                foreignField : "_id",
+                as : "channelInfo", 
+                pipeline : [
+                    {
+                        $project : {
+                            _id : 1,
+                            username : 1,
+                            fullname : 1,
+                            avatar : 1
+                        }
+                    }
+                ]
+            }, 
+        }, {
+            $unwind : "$channelInfo"
+        },  {
+            $project : {
+                _id : 1,
+                subscriber : 1,
+                channel : 1,
+                channelInfo : "$channelInfo"
+            }
+        }
+    ])
+
+    if (!subscribedChannels) {
+        throw new ApiError(500, "Unable find to find subscriber list")
+    }
+
+    const channelCount = subscribedChannels.length;
+
+    res.status(200).json(
+        new ApiResponse(200, {
+            channelCount,
+            subscribedChannels
+        }, "Subscribed channels fetched successfully" )
+    )
+} catch (error) {
+    throw new ApiError(error.statusCode || 500, error.message || "Something went wrong while fetching subscribed channels")
+}
 })
 
 export {
